1ffca99df11e3cd5a7a2b1f20b82109e6efb9b78
John Gibson <jg20019@gmail.com>
2020-08-12 06:56:53 -0400

    Completed exercises 1.6 and 1.8

--- chapter_1/exercises.scm
+++ chapter_1/exercises.scm
@@ -76,8 +76,57 @@
 ;     (p)) => 0
 
 
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)))
 
+(define (improve guess x)
+  (average guess (/ x guess)))
 
+(define (average x y)
+  (/ (+ x y) 2))
 
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+Exercise 1.6
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+	(else else-clause)))
+
+(define (ex-sqr-iter guess x)
+  (new-if (good-enough? guess x)
+	  guess
+	  (ex-sqr-iter (improve guess x) x)))
+
+; Since new-if is a function all of its arguments get evaluated, include the recursive call 
+; to ex-sqrt-iter. Instead of computing the sqrt it gets stuck in an infinite loop 
+
+; Exercise 1.8 
+
+(define (cube-root x) 
+  (cube-root-iter 1.0 x))
+
+(define (cube-root-iter guess x)
+  (if (good-enough-cube? guess x) 
+      guess
+      (cube-root-iter (improve-cube guess x) x)))
+
+(define (good-enough-cube? guess x) 
+  (< (abs (- (cube guess) x)) 0.001))
+
+(define (improve-cube guess x) 
+  (/ (+ (/ x (square guess))  (* 2 guess))
+     3))
+
+(define (square x) 
+  (* x x))
+
+(define (cube x) 
+  (* x x x))
 
 
