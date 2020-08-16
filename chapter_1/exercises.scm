ff93020361d82f64f06a36de7c460176103c3ce0
John Gibson <jg20019@gmail.com>
2020-08-16 04:48:20 -0400

    Remove .swp file; Complete exercise 1.10

--- chapter_1/exercises.scm
+++ chapter_1/exercises.scm
@@ -130,3 +130,148 @@ Exercise 1.6
   (* x x x))
 
 
+; Exercise 1.9 
+(define (plus-1 a b)
+  (if (= a 0) 
+      b
+      (inc (plus-1 (dec a) b))))
+
+(plus-1 3 4) 
+(inc (plus-1 2 4))
+(inc (inc (plus-1 1 4)))
+(inc (inc (inc (plus-1 0 4))))
+(inc (inc (inc 4)))
+(inc (inc 5))
+(inc 6)
+7 
+
+; This function is a recursive process because it defers the inc operations 
+; until it reaches the base case. 
+
+(define (plus-2 a b) 
+  (if (= a 0) 
+      b 
+      (plus-2 (dec a) (inc b))))
+
+(plus-2 3 4)
+(plus-2 2 5)
+(plus-2 1 6)
+(plus-2 0 7)
+7 
+; This function is an iterative process. 
+
+; Exercise 1.10 
+; Ackermann's function 
+
+(define (A x y)
+  (cond ((= y 0) 0)
+	((= x 0) (* 2 y))
+	((= y 1) 2)
+	(else (A (- x 1)
+		 (A x (- y 1))))))
+
+
+
+
+
+(A 1 10) 
+(A 0 (A 1 9))
+(A 0 (A 0 (A 1 8)))
+(A 0 (A 0 (A 0 (A 1 7))))
+(A 0 (A 0 (A 0 (A 0 (A 1 6)))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 1 5))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 4)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 3))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 1))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 8)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 32)))))
+(A 0 (A 0 (A 0 (A 0 64))))
+(A 0 (A 0 (A 0 128)))
+(A 0 (A 0 256))
+(A 0 512)
+1024 
+
+
+(A 2 4)
+(A 1 (A 2 3))
+(A 1 (A 1 (A 2 2)))
+(A 1 (A 1 (A 1 (A 2 1))))
+(A 1 (A 1 (A 1 2)))
+(A 1 (A 1 (A 0 (A 1 1))))
+(A 1 (A 1 (A 0 2)))
+(A 1 (A 1 4))
+(A 1 (A 0 (A 1 3)))
+(A 1 (A 0 (A 0 (A 1 2))))
+(A 1 (A 0 (A 0 (A 0 (A 1 1)))))
+(A 1 (A 0 (A 0 (A 0 2))))
+(A 1 (A 0 (A 0 4)))
+(A 1 (A 0 8))
+(A 1 16)
+(A 0 (A 1 15))
+(A 0 (A 0 (A 1 14)))
+(A 0 (A 0 (A 0 (A 1 13))))
+(A 0 (A 0 (A 0 (A 0 (A 1 12)))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 1 11))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 10)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 9))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 8)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 7))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 6)))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 5))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 4)))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 3)))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 2))))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 1 1))))))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 2))))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 4))))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 16))))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 32)))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 64))))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 128)))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 256))))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 (A 0 512)))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 (A 0 1024))))))
+(A 0 (A 0 (A 0 (A 0 (A 0 2048)))))
+(A 0 (A 0 (A 0 (A 0 4096))))
+(A 0 (A 0 (A 0 8192)))
+(A 0 (A 0 16384))
+(A 0 32768)
+65536 
+
+(A 3 3)
+(A 2 (A 3 2))
+(A 2 (A 2 (A 3 1)))
+(A 2 (A 2 2))
+(A 2 (A 1 (A 2 1)))
+(A 2 (A 1 2))
+(A 2 (A 0 (A 1 1)))
+(A 2 (A 0 2))
+(A 2 4) => 65536
+
+(define (f n) (A 0 n))
+; f(n) = 2n 
+
+(define (g n) (A 1 n))
+; g(n) = 2^n 
+
+(define (h n) (A 2 n))
+; h(0) = 0
+; h(1) = 2
+; h(n) = 2^(h(n-1))
+
+(define (h-prime n) 
+  ; this is what h does. I don't know how else to represent this mathematically, 
+  ; I could probably make this quicker by using iteration
+  (cond ((= n 0) 0)
+        ((= n 1) 2)
+        (else (expt 2 (h-prime (- n 1))))))
+
+(and (= (h 0)  (h-prime 0))
+     (= (h 1) (h-prime 1)) 
+     (= (h 2) (h-prime 2))
+     (= (h 3) (h-prime 3)) 
+     (= (h 4) (h-prime 4)))
